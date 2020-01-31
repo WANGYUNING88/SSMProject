9b10c835637e06c2fb02e624bb7d11072e402652
王余柠 <247197442@qq.com>
2020-01-31 20:13:05 +0800

    resources

--- springmvcday01_01_start/src/main/java/com/wang/controller/HelloController.java
+++ springmvcday01_01_start/src/main/java/com/wang/controller/HelloController.java
@@ -1,9 +1,15 @@
 package com.wang.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * //控制器类
  */
+@Controller
 public class HelloController {
+
+    @RequestMapping
     public String sayHello(){
         System.out.println("Hello SpringMVC");
         return null;

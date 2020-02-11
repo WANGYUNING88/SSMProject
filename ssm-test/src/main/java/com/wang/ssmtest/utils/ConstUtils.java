d1ce17529824e780803c968a4c428e2256ade8a4
wangyuning88 <19970305.wang>
2020-02-11 12:58:09 +0800

    new

--- ssm-test/src/main/java/com/wang/ssmtest/utils/ConstUtils.java
+++ ssm-test/src/main/java/com/wang/ssmtest/utils/ConstUtils.java
@@ -1,7 +1,7 @@
 package com.wang.ssmtest.utils;
 
 public class ConstUtils {
-    public static final String UPLOADPATH = "E:\\img\\uploads\\";
+    public static final String UPLOADPATH = "G:\\img\\uploads\\";
     public static final String DEFAULTIMAGE ="system\\" + "default.jfif";
     public static final String DEFAULT = "default";
 }

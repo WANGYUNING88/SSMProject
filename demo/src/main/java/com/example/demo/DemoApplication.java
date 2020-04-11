88731aef9bdf2ac80e3d1fa69a6e66ec5b02d384
wangyuning88 <19970305.wang>
2020-04-11 21:02:10 +0800

    springboot

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//本身就是Spring的一个组件
+
+//程序主入口
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}

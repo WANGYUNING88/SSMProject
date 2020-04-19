1a2305875532c95f272f6a5f1b362291ff23f862
wangyuning88 <19970305.wang>
2020-04-19 10:44:22 +0800

    springboot

---
+++ springboot-security/src/main/java/com/wang/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.wang.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    //授权
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //首页所有人都可以访问 功能页只有对应权限的人才能访问
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("vip1")
+                .antMatchers("/level2/**").hasRole("vip2")
+                .antMatchers("/level3/**").hasRole("vip3");
+
+        //没有权限 默认到 登录 页面
+        http.formLogin();
+
+        //注销 -成功跳到首页
+        //防止网站工具 get post
+        http.csrf().disable();//关闭csrf功能
+        http.logout().logoutSuccessUrl("/");
+
+
+    }
+
+    //认证
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("wangyuning1").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1","vip2","vip3")
+                .and()
+                .withUser("wangyuning2").password(new BCryptPasswordEncoder().encode("123456")).roles("vip1","vip2")
+                .and()
+                .withUser("wangyuning3").password(new BCryptPasswordEncoder().encode("123456")).roles("vip3");
+    }
+}
---
+++ springboot-security/src/test/java/com/wang/SpringbootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

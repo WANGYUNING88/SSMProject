5ebab16d0160fa9e15619aca3852b882b16895c6
wangyuning88 <19970305.wang>
2020-05-23 18:42:36 +0800

    vue

---
+++ vuejs/hello-vue/src/views/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <el-form ref="loginForm" :model="form" :rules="rules" label-width="80px" class="login-box">
+      <h3 class="login-title">欢迎登陆</h3>
+      <el-form-item label="账号" prop="username">
+        <el-input type="text" placeholder="请出入账号" v-model="form.username"/>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" placeholder="请出入密码" v-model="form.password"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit('loginForm')">登录</el-button>
+      </el-form-item>
+    </el-form>
+    <el-dialog
+      title="温馨提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose">
+      <span>请出入账号密码</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button type="peimary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    data() {
+      return {
+        form: {
+          username: '',
+          password: ''
+        },
+        //表单验证，需要el-form-item 元素中增加 prop 属性
+        rules:  {
+          username: [
+            {requir}
+          ]
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
